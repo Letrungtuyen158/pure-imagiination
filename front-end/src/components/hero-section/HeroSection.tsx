7c82f65a3ce21266d4e0af4a463267577dae41f8
tuyen <tuyentrungle158@gmail.com>
2024-06-11 22:56:56 +0700

    feat: update sr

--- front-end/src/components/hero-section/HeroSection.tsx
+++ front-end/src/components/hero-section/HeroSection.tsx
@@ -7,38 +7,17 @@ const HeroSection = () => {
   return (
     <div className="relative  h-[400px] lg:h-[670px] " id="home">
       <div className="absolute top-0 left-0 w-full h-full -z-10">
-        <Image
-          src={banner}
-          fill
-          style={{ objectFit: "cover" }}
-          placeholder="blur"
-          quality={100}
-          alt="Background Image"
-          priority
-          className="absolute inset-0"
-        />
+        <Image src={banner} fill style={{ objectFit: "cover" }} placeholder="blur" quality={100} alt="Background Image" priority className="absolute inset-0" />
       </div>
       <Container>
         <div className="relative pt-32 lg:pt-36 ml-auto ">
           <TextAnimation />
-          <button
-            type="button"
-            className="flex p-2 text-center mx-auto items-center gap-2 rounded-full bg-opacity-30 backdrop-blur-md"
-          >
-            <p className="text-white text-center  text-2xl font-bold leading-normal capitalize">
-              STAR YOUR DREAM
-            </p>
+          <button type="button" className="flex p-2 text-center mx-auto items-center gap-2 rounded-full bg-opacity-30 backdrop-blur-md">
+            <p className="text-white text-center  text-2xl font-bold leading-normal capitalize">STAR YOUR DREAM</p>
             <div className="w-9 h-9 relative">
               <div className="w-9 h-9 left-0 top-0 absolute bg-[#FF3EA5] rounded-[100px]" />
               <div className="w-3 h-3 left-[12px] top-[12px] absolute justify-center items-center inline-flex">
-                <svg
-                  width="12"
-                  height="12"
-                  viewBox="0 0 12 12"
-                  fill="none"
-                  className="group-hover:rotate-45 duration-300"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" className="group-hover:rotate-45 duration-300" xmlns="http://www.w3.org/2000/svg">
                   <g clipPath="url(#clip0_10_39)">
                     <path
                       d="M11.227 0H2.15898C1.73242 0 1.38555 0.346875 1.38555 0.773438C1.38555 1.2 1.73242 1.54688 2.15898 1.54688H9.35664L0.227734 10.6781C-0.0746094 10.9805 -0.0746094 11.4703 0.227734 11.7727C0.530078 12.075 1.01992 12.075 1.32227 11.7727L10.4512 2.64141V9.83906C10.4512 10.2656 10.798 10.6125 11.2246 10.6125C11.6512 10.6125 11.998 10.2656 11.998 9.83906V0.773438C12.0004 0.346875 11.6535 0 11.227 0Z"
@@ -61,4 +40,3 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
-
--- front-end/src/components/our-pure-dreamers-section/OurPureDreamers.tsx
+++ front-end/src/components/our-pure-dreamers-section/OurPureDreamers.tsx
@@ -9,51 +9,23 @@ import Image from "next/image";
 
 const CustomLeftArrow = ({ onClick }: ArrowProps) => (
   <button onClick={onClick} className="custom-left-arrow ">
-    <svg
-      width="100"
-      height="100"
-      viewBox="0 0 100 100"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M62.5 75L37.5 50L62.5 25"
-        stroke="#65758B"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M62.5 75L37.5 50L62.5 25" stroke="#65758B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   </button>
 );
 
 const CustomRightArrow = ({ onClick }: ArrowProps) => (
   <button onClick={onClick} className="custom-right-arrow ">
-    <svg
-      width="100"
-      height="100"
-      viewBox="0 0 100 100"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M37.5 25L62.5 50L37.5 75"
-        stroke="#CC3284"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+    <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M37.5 25L62.5 50L37.5 75" stroke="#CC3284" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   </button>
 );
 
 const CustomDot = ({ onClick, active }: DotProps) => (
   <li className={`custom-dot ${active ? "active" : ""}`} onClick={onClick}>
-    {active ? (
-      <div className="w-3.5 h-3.5 bg-pink-600 rounded-[100px]" />
-    ) : (
-      <div className="w-3.5 h-3.5 bg-slate-300 rounded-[100px]" />
-    )}
+    {active ? <div className="w-3.5 h-3.5 bg-pink-600 rounded-[100px]" /> : <div className="w-3.5 h-3.5 bg-slate-300 rounded-[100px]" />}
   </li>
 );
 
@@ -76,25 +48,15 @@ export default function CarouselSlide() {
   return (
     <div className="bg-white relative py-10">
       <div className="absolute top-0 right-0">
-        <svg
-          width="1920"
-          height="88"
-          viewBox="0 0 1920 88"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="1920" height="88" viewBox="0 0 1920 88" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0 0L1920 88V0H0Z" fill="#FFECF6" />
         </svg>
       </div>
 
       <Container>
         <div className="mt-28 mb-4 text-center">
-          <span className="text-zinc-900 text-6xl font-extrabold  leading-[72px]">
-            OUR{" "}
-          </span>
-          <span className="text-pink-500 text-6xl font-extrabold  leading-[72px]">
-            PURE DREAMERS
-          </span>
+          <span className="text-zinc-900 text-6xl font-extrabold  leading-[72px]">OUR </span>
+          <span className="text-pink-500 text-6xl font-extrabold  leading-[72px]">PURE DREAMERS</span>
         </div>
 
         <Carousel
@@ -107,44 +69,23 @@ export default function CarouselSlide() {
           customLeftArrow={<CustomLeftArrow />}
           customRightArrow={<CustomRightArrow />}
           customDot={<CustomDot />}
-          dotListClass="custom-dot-list-style"
-        >
+          dotListClass="custom-dot-list-style">
           <div className="px-4 py-8 flex items-center gap-10 sm:h-[400px] sm:px-10 xl:px-60">
-            <Image
-              className="rounded-full"
-              width={400}
-              height={400}
-              src="https://via.placeholder.com/400x400"
-              alt="Elly Image"
-            />
+            <Image className="rounded-full" width={400} height={400} src="https://via.placeholder.com/400x400" alt="Elly Image" />
             <div className="flex flex-col items-start gap-6">
               <div className="flex flex-col items-start gap-2">
-                <div className="text-center text-pink-500 text-[24px] sm:text-[32px] font-bold  leading-[28px] sm:leading-[38.40px]">
-                  Elly
-                </div>
-                <div className="text-center text-gray-700 text-lg sm:text-2xl font-medium  leading-[24px] sm:leading-[28.80px]">
-                  Staff of Pure
-                </div>
+                <div className="text-center text-pink-500 text-[24px] sm:text-[32px] font-bold  leading-[28px] sm:leading-[38.40px]">Elly</div>
+                <div className="text-center text-gray-700 text-lg sm:text-2xl font-medium  leading-[24px] sm:leading-[28.80px]">Staff of Pure</div>
               </div>
               <div className="text-slate-500 text-base sm:text-lg font-medium  leading-[24px] sm:leading-[28.80px]">
-                Ely It Clean aka Ely Nguyen is a beloved creator known for her
-                captivating charm and innovative culinary content. Her
-                delightful personality and fresh approach to cooking have
-                resonated with a wide audience. We have proudly supported Ely
-                since her early days, and in just one year, she has achieved
-                remarkable success with 1 million followers, while making a
+                Ely It Clean aka Ely Nguyen is a beloved creator known for her captivating charm and innovative culinary content. Her delightful personality and fresh approach to cooking have
+                resonated with a wide audience. We have proudly supported Ely since her early days, and in just one year, she has achieved remarkable success with 1 million followers, while making a
                 positive impact on her community.
               </div>
               <div className="flex justify-start items-center gap-4">
                 <div className="w-10 h-10 sm:w-12 sm:h-12 bg-pink-100 rounded-full relative">
                   <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <g clipPath="url(#clip0_1_253)">
                         <path
                           d="M13.8587 24V13.0533H17.5316L18.0826 8.78588H13.8587V6.06176C13.8587 4.82664 14.2003 3.98492 15.9734 3.98492L18.2313 3.98399V0.167076C17.8408 0.116334 16.5005 0 14.9405 0C11.683 0 9.45282 1.98836 9.45282 5.63912V8.78588H5.7688V13.0533H9.45282V24H13.8587Z"
@@ -161,13 +102,7 @@ export default function CarouselSlide() {
                 </div>
                 <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border border-slate-500 relative">
                   <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M12 8.33203C9.97603 8.33203 8.33203 9.97603 8.33203 12C8.33203 14.024 9.97603 15.672 12 15.672C14.024 15.672 15.672 14.024 15.672 12C15.672 9.97603 14.024 8.33203 12 8.33203Z"
                         fill="#15191E"
@@ -181,13 +116,7 @@ export default function CarouselSlide() {
                 </div>
                 <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border border-slate-500 relative">
                   <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         d="M18.3263 1.90393H21.6998L14.3297 10.3274L23 21.7899H16.2112L10.894 14.838L4.80995 21.7899H1.43443L9.31743 12.78L1 1.90393H7.96111L12.7674 8.25826L18.3263 1.90393ZM17.1423 19.7707H19.0116L6.94539 3.81706H4.93946L17.1423 19.7707Z"
                         fill="#15191E"
@@ -197,13 +126,7 @@ export default function CarouselSlide() {
                 </div>
                 <div className="w-10 h-10 sm:w-12 sm:h-12 rounded-full border border-slate-500 relative">
                   <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
                         fillRule="evenodd"
                         clipRule="evenodd"
@@ -221,4 +144,3 @@ export default function CarouselSlide() {
     </div>
   );
 }
-
