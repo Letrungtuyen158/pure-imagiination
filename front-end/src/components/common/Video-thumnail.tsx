9f1a0eedfe491c49ab63b9efe24c9670bfea2077
tuyen <tuyentrungle158@gmail.com>
2024-06-13 23:20:57 +0700

    feat: update video

--- front-end/src/components/common/Video-thumnail.tsx
+++ front-end/src/components/common/Video-thumnail.tsx
@@ -1,9 +1,6 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import { CTA_DATA } from "../cta-section/data";
 import Play from "./icon/Play";
-
 const VideoThumbnail = () => {
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
 
@@ -16,16 +13,19 @@ const VideoThumbnail = () => {
   };
 
   return (
-    <div className="relative">
-      <div onClick={handleThumbnailClick}>
-        <Image alt={CTA_DATA.title} placeholder="blur" priority className="object-center rounded-[2rem]" quality={100} width={616} height={413} src={CTA_DATA.banner} />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="relative ">
+      <div>
+        <video width={616} height={346} className="object-center rounded-[2rem] " autoPlay muted loop playsInline>
+          <source src="/Pure_Imagination.mp4" type="video/mp4" />
+          <source src="/Pure_Imagination.webm" type="video/webm" />
+        </video>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer" onClick={handleThumbnailClick}>
           <Play />
         </div>
       </div>
       {isVideoLoaded && (
         <div className="fixed w-full h-screen inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
-          <div className="relative h-[800px] w-[1000px]  max-w-3xl">
+          <div className="relative h-[800px] w-[1000px] max-w-3xl">
             <button onClick={handleCloseClick} className="absolute top-0 right-0 bg-white rounded-full w-8 h-8">
               ✕
             </button>
