07843ad3ed0c814ef15ce9c813d7305a7ef9fed0
tuyen <tuyentrungle158@gmail.com>
2024-06-22 22:18:58 +0700

    fix bug color btn

--- front-end/src/components/hero-section/HeroSection.tsx
+++ front-end/src/components/hero-section/HeroSection.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import banner from "../../../public/banner.png";
 import Container from "../common/container";
 import TextAnimation from "./TextAnimation";
+import CtaButton from "../common/cta-button";
 
 const HeroSection = () => {
   return (
@@ -13,33 +14,13 @@ const HeroSection = () => {
       <Container>
         <div className="relative pt-32 lg:pt-36 ml-auto ">
           <TextAnimation />
-          <button type="button" className="flex px-2 py-1 text-center group mx-auto items-center gap-2  border-yellow-400 hover:bg-sky-400 hover:border-sky-400 border-[3px]  rounded-full">
-            <p className="text-white text-center text-xl lg:text-2xl font-extrabold leading-normal capitalize">START YOUR DREAM</p>
-            <div className="w-9 h-9 relative">
-              <div className="w-9 h-9 left-0 top-0 absolute bg-[#FF3EA5] rounded-[100px]" />
-              <div className="w-3 h-3 left-[12px] top-[12px] absolute justify-center items-center inline-flex">
-                <svg
-                  width="12"
-                  height="12"
-                  viewBox="0 0 12 12"
-                  fill="none"
-                  className="group-hover:rotate-[135deg] absolute duration-300 inset-0 m-auto flex items-center justify-center"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <g clipPath="url(#clip0_10_39)">
-                    <path
-                      d="M11.227 0H2.15898C1.73242 0 1.38555 0.346875 1.38555 0.773438C1.38555 1.2 1.73242 1.54688 2.15898 1.54688H9.35664L0.227734 10.6781C-0.0746094 10.9805 -0.0746094 11.4703 0.227734 11.7727C0.530078 12.075 1.01992 12.075 1.32227 11.7727L10.4512 2.64141V9.83906C10.4512 10.2656 10.798 10.6125 11.2246 10.6125C11.6512 10.6125 11.998 10.2656 11.998 9.83906V0.773438C12.0004 0.346875 11.6535 0 11.227 0Z"
-                      fill="white"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_10_39">
-                      <rect width="12" height="12" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </div>
-            </div>
-          </button>
+          <CtaButton
+            fill="#fff"
+            svgClass="bg-[#FF3EA5]"
+            classNameContainer="flex px-2 py-1 text-center group mx-auto items-center gap-2 bg-transparent border-yellow-400 hover:bg-sky-400 hover:border-sky-400 border-[3px]  rounded-full"
+            className="text-base leading-normal bg-[FF3EA5] tracking-wide">
+            START YOUR DREAM
+          </CtaButton>
         </div>
       </Container>
     </div>
