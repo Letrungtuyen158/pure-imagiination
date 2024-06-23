ef67a2e568a2150901cb313246cefb23ca07ea01
tuyen <tuyentrungle158@gmail.com>
2024-06-23 14:04:07 +0700

    fix css

--- front-end/src/components/our-pure-dreamers-section/OurPureDreamers.tsx
+++ front-end/src/components/our-pure-dreamers-section/OurPureDreamers.tsx
@@ -85,7 +85,7 @@ export default function CarouselSlide() {
                   <div className="flex flex-col lg:items-start max-w-[768px] h-[400px] gap-2">
                     <p className="text-center text-primary-pink text-[24px] sm:text-[32px] font-black tracking-wide leading-[28px] sm:leading-[38.40px]">{creator.name}</p>
                     <p className="text-center text-gray-700 mb-4 text-lg sm:text-2xl font-extrabold leading-[24px] tracking-wide sm:leading-[28.80px]">{creator.position}</p>
-                    <p className="text-black-base text-base sm:text-lg lg:text-[0.9rem] font-semibold leading-[24px] tracking-wide xl:leading-[28.80px]">{creator.introduce}</p>
+                    <p className="text-black-base text-base sm:text-lg lg:text-[0.9rem] xl:text-[1.125rem] font-semibold leading-[24px] tracking-wide xl:leading-[28.80px]">{creator.introduce}</p>
                   </div>
                   <div className="flex justify-start items-center gap-4">
                     <a href={creator.href_fb} target="_blank" className="group w-10 h-10 sm:w-12 sm:h-12 rounded-full border border-slate-500 hover:border-none hover:bg-pink-100 relative">
