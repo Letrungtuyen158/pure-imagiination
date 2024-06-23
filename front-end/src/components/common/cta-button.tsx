be3cfc97471c3ad94d781e05c5d8bee5afc9f726
tuyen <tuyentrungle158@gmail.com>
2024-06-23 12:26:47 +0700

    fix arrow

--- front-end/src/components/common/cta-button.tsx
+++ front-end/src/components/common/cta-button.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { cn } from "@/utils/cn";
 import React from "react";
+import Image from "next/image";
+import arrow from "./arrow.png";
 
 export default function CtaButton({
   children,
@@ -23,25 +25,7 @@ export default function CtaButton({
       <div className="w-9 h-9 relative">
         <div className={cn("w-9 h-9 left-0 top-0 absolute bg-white rounded-[100px]", svgClass)} />
         <div className="w-3 h-3 left-[12px] top-[12px] absolute justify-center items-center inline-flex">
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            className="group-hover:rotate-[135deg] absolute duration-300 inset-0 m-auto flex items-center justify-center"
-            xmlns="http://www.w3.org/2000/svg">
-            <g clipPath="url(#clip0_10_39)">
-              <path
-                d="M11.227 0H2.15898C1.73242 0 1.38555 0.346875 1.38555 0.773438C1.38555 1.2 1.73242 1.54688 2.15898 1.54688H9.35664L0.227734 10.6781C-0.0746094 10.9805 -0.0746094 11.4703 0.227734 11.7727C0.530078 12.075 1.01992 12.075 1.32227 11.7727L10.4512 2.64141V9.83906C10.4512 10.2656 10.798 10.6125 11.2246 10.6125C11.6512 10.6125 11.998 10.2656 11.998 9.83906V0.773438C12.0004 0.346875 11.6535 0 11.227 0Z"
-                fill={fill}
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_10_39">
-                <rect width="12" height="12" fill={fill} />
-              </clipPath>
-            </defs>
-          </svg>
+          <Image src={arrow} alt={"sads"} className="absolute group-hover:rotate-[135deg] duration-300 w-[14px] h-[14px]" />
         </div>
       </div>
     </button>
